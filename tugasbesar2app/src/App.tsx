06eb3f6c816abc98d1368b0889258a96d974aaa5
staroceanre <renomaulana234@gmail.com>
2025-07-07 22:50:02 +0700

    Update Halaman dashboard - Pakai template lain saja

--- tugasbesar2app/src/App.tsx
+++ tugasbesar2app/src/App.tsx
@@ -1,34 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Layout from "./components/Layout"
+import Login from "./pages/Login"
+import Dashboard from "./pages/dashboard"
+import Ddosen from "./pages/ddosen"
+import Dkelas from "./pages/dkelas"
+import Dmatakuliah from "./pages/dmatakuliah"
+import DashboardLayout from "./components/DashboardLayout"
+import Register from "./pages/Register"
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <BrowserRouter>
+    <Routes>
+      <Route element={<Layout/>}>
+        <Route path="/" element={<Login/>}/>
+        <Route path="/Register" element={<Register/>}/>
+        <Route path="/dashboard" element={<Dashboard/>}/>
+      </Route>
+      <Route element={<DashboardLayout/>}>
+        <Route path="/dashboard" element={<Dashboard/>}/>
+        <Route path="/home" element={<Ddosen/>}/>
+        <Route path="/home" element={<Dkelas/>}/>
+        <Route path="/home" element={<Dmatakuliah/>}/>
+      </Route>
+    </Routes>
+    </BrowserRouter>
   )
 }
 
